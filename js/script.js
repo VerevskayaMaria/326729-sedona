79bf30d3b1a574461a0a8273bc7e912e2d8b35ce
VerevskayaMaria <verevskaya94@mail.ru>
2017-02-17 07:54:05 +1000

    //

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var search = document.querySelector(".search-index .btn-alt"); 
 var form = document.querySelector(".search form"); 
 
-search.addEventListener("click", function(event) { 
+search.addEventListener("click", function(event) {
+  event.preventDefault();
   if (form.classList.contains("form-search-show")) { 
     form.classList.remove("form-search-show"); 
   } 
